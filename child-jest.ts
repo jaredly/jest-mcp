844445adcefd21b6ce79228abbfc05262f0882b2
Jared Forsyth <jared@jaredforsyth.com>
2025-07-09 13:59:15 -0600

    much more robust

---
+++ child-jest.ts
@@ -0,0 +1,53 @@
+import {existsSync, statSync} from 'fs';
+import {runCLI} from 'jest';
+
+process.on('message', async ({cwd, testPattern}) => {
+    if (!existsSync(cwd)) {
+        return process.send?.({
+            type: 'text',
+            text: `Error: ${cwd} does not exist`,
+        });
+    }
+    if (!statSync(cwd).isDirectory()) {
+        return process.send?.({
+            type: 'text',
+            text: `Error: ${cwd} is not a directory`,
+        });
+    }
+    process.chdir(cwd);
+    try {
+        const result = await runCLI(
+            {
+                _: testPattern ? [testPattern] : [],
+                coverage: true,
+                json: true,
+                $0: 'jest-mcp',
+                runInBand: true,
+                // testNamePattern: testPattern ? testPattern : undefined,
+                silent: true,
+                rootDir: cwd,
+            },
+            [cwd],
+        );
+
+        const {results} = result;
+        process.send?.({
+            type: 'text',
+            text: JSON.stringify({
+                success: results.success,
+                numPassedTests: results.numPassedTests,
+                numFailedTests: results.numFailedTests,
+                testResults: results.testResults,
+                coverageMap: results.coverageMap,
+            }),
+        });
+    } catch (err) {
+        return process.send?.({
+            type: 'text',
+            text: `Error running jest: ${err.message}`,
+        });
+    }
+});
+/*
+/Users/jared/clone/apps/artswap/backend
+*/
--- server.ts
+++ server.ts
@@ -3,6 +3,8 @@ import {McpServer, ResourceTemplate} from '@modelcontextprotocol/sdk/server/mcp.
 import {StdioServerTransport} from '@modelcontextprotocol/sdk/server/stdio.js';
 import {runCLI} from 'jest';
 import {z} from 'zod';
+import {fork} from 'child_process';
+import {resolve} from 'path';
 
 // Create an MCP server
 const server = new McpServer({
@@ -19,58 +21,91 @@ server.registerTool(
         inputSchema: {cwd: z.string(), testPattern: z.string().optional()},
     },
     async ({cwd, testPattern}) => {
-        process.chdir(cwd);
-        const result = await runCLI(
-            {
-                _: [],
-                coverage: true,
-                json: true,
-                $0: 'jest-mcp',
-                runInBand: true,
-                testNamePattern: testPattern ? testPattern : undefined,
-                silent: true,
-            },
-            [cwd],
-        );
+        return new Promise((res) => {
+            console.log('forking');
+            const child = fork(resolve('./child-jest.ts'), [], {
+                stdio: ['pipe', 'pipe', 'pipe', 'ipc'],
+            });
 
-        const {results} = result;
-        return {
-            content: [
-                {
-                    type: 'text',
-                    text: JSON.stringify({
-                        success: results.success,
-                        numPassedTests: results.numPassedTests,
-                        numFailedTests: results.numFailedTests,
-                        testResults: results.testResults,
-                        coverageMap: results.coverageMap,
-                    }),
-                },
-            ],
-        };
+            const out = [];
+            child.stdout?.setEncoding('utf8');
+            child.stdout?.on('data', (data) => {
+                out.push({type: 'text', text: `stdout: ${data.trim()}`});
+            });
+            child.stderr?.setEncoding('utf8');
+            child.stderr?.on('data', (data) => {
+                if (data.includes(`ExperimentalWarning: Type Stripping`)) {
+                    return; // ignore that one
+                }
+                out.push({type: 'text', text: `stderr: ${data.trim()}`});
+            });
 
-        //     ({
-        //     content: [{type: 'text', text: String(a + b)}],
-        // })
+            child.send({cwd, testPattern});
+
+            // happy path, no need to bother with stdout/stderr
+            child.on('message', (message) => {
+                res({content: [message]});
+                child.kill();
+            });
+
+            child.on('exit', (err) => {
+                res({
+                    content: [
+                        ...out,
+                        {
+                            type: 'text',
+                            text: JSON.stringify({
+                                error: 'Exited without responding',
+                            }),
+                        },
+                    ],
+                });
+            });
+
+            child.on('error', (err) => {
+                res({
+                    content: [
+                        ...out,
+                        {
+                            type: 'text',
+                            text: JSON.stringify({error: err.message}),
+                        },
+                    ],
+                });
+            });
+        });
+
+        // process.chdir(cwd);
+        // const result = await runCLI(
+        //     {
+        //         _: testPattern ? [testPattern] : [],
+        //         coverage: true,
+        //         json: true,
+        //         $0: 'jest-mcp',
+        //         runInBand: true,
+        //         // testNamePattern: testPattern ? testPattern : undefined,
+        //         silent: true,
+        //         rootDir: cwd,
+        //     },
+        //     [cwd],
+        // );
+
+        // const {results} = result;
+        // return {
+        //     content: [
+        //         {
+        //             type: 'text',
+        //             text: JSON.stringify({
+        //                 success: results.success,
+        //                 numPassedTests: results.numPassedTests,
+        //                 numFailedTests: results.numFailedTests,
+        //                 testResults: results.testResults,
+        //                 coverageMap: results.coverageMap,
+        //             }),
+        //         },
+        //     ],
+        // };
     },
-);
-
-// Add a dynamic greeting resource
-server.registerResource(
-    'greeting',
-    new ResourceTemplate('greeting://{name}', {list: undefined}),
-    {
-        title: 'Greeting Resource', // Display name for UI
-        description: 'Dynamic greeting generator',
-    },
-    async (uri, {name}) => ({
-        contents: [
-            {
-                uri: uri.href,
-                text: `Hello, ${name}!`,
-            },
-        ],
-    }),
 );
 
 // Start receiving messages on stdin and sending messages on stdout
